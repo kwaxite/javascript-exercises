57e7cd3c3cd02c80ae3c6f20c55092106b0b7ed1
kwaxite <146014461+kwaxite@users.noreply.github.com>
2023-11-20 20:31:19 +0000

    test repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    var numStr = ''
+    if (num < 0){return 'ERROR'}
+    for (let i=0; i<num; i++){
+        
+        numStr = numStr + string
+    }
+    return numStr
 };
 
 // Do not edit below this line
