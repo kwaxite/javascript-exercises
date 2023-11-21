7b2d94527810a6d41da25cce2649a5848e7ae298
kwaxite <146014461+kwaxite@users.noreply.github.com>
2023-11-21 15:51:44 +0000

    create and test sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(sta, end) {
+    total = 0
+    if (typeof sta !== 'number' | typeof end !== 'number' | end < 0 | sta < 0){
+        return 'ERROR'
+    }
+    if (sta > end){
+        let temp = sta
+        sta = end
+        end = temp
+    }
+    for (let i=sta; i<end+1; i++){
+        total += i
+    }
+    return total
 };
 
 // Do not edit below this line
